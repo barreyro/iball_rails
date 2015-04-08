916ffdecde87ce73c1eec69898f1f09f3a232aa4
Stephanie <stephaniebarreyro@gmail.com>
2015-04-08 00:58:06 -0700

    Cont'd User migration.

--- db/migrate/20150408074836_create_users.rb
+++ db/migrate/20150408074836_create_users.rb
@@ -2,8 +2,15 @@ class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string  :username
+      t.string  :first_name
+      t.string  :last_name
       t.string  :email
       t.string  :password_digest
+      t.string  :city
+      t.string  :state
+      t.integer :age
+      t.integer :games_total
+      t.integer :games_won
 
       t.timestamps null: false
     end
