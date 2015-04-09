3648e78ff9586f2868f2eabe55a44a366feff090
Stephanie <stephaniebarreyro@gmail.com>
2015-04-09 00:46:12 -0700

    Add session routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   resources :users, :games, :comments
   root :to => 'users#index'
+  post '/logout' => 'sessions#logout'
+  post '/login' => 'sessions#login'
 
 end
