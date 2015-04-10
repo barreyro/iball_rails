99e2ba837718207585ba2be42d3ad1f54639ff1b
Stephanie <stephaniebarreyro@gmail.com>
2015-04-09 23:42:52 -0700

    Correct session routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,24 @@
 Rails.application.routes.draw do
   resources :users, :games, :comments
   root :to => 'users#index'
-  post '/logout' => 'sessions#logout'
-  post '/login' => 'sessions#login'
+  get "login", to: "sessions#login"
+  post "login", to: "sessions#create_session"
+  get "logout", to: "sessions#logout"
 
+  root 'users#index'
+
+  get "login", to: "sessions#login"
+  post "login", to: "sessions#create_session"
+  get "logout", to: "sessions#logout"
+
+  get "signup", to: "users#new", as: "signup"
+  post "signup", to: "users#create"
+
+  resources :users do
+    resources :events
+    resources :friendships
+    resources :comments
+  end
+
+  resources :lineages
 end
