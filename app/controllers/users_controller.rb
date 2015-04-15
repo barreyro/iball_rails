aabd643257489452298e97393b276599b94ca6b5
Stephanie <stephaniebarreyro@gmail.com>
2015-04-15 16:03:15 -0700

    Assign session ID to user ID.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,14 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      session[:user_id] = user.user_id
+      session[:user_id] = @user.id
       redirect_to user_path(@current_user)
     else
       render :new
     end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :city, :state, :age, :bio)
   end
 end
