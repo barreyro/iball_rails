993e5f50ada760d4900dfd2ebbc846df0d6a7991
Stephanie <stephaniebarreyro@gmail.com>
2015-04-15 18:00:24 -0700

    Render sessions template.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,22 @@
 class SessionsController < ApplicationController
-  def create_session
+  def login
   end
 
+  def begin
+    user = Session.login_by_username_or_email(params[:user_input])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:success] = "Welcome, #{user.username}!"
+      redirect_to root_path
+    else
+      flash[:failure] = "Login failed."
+      render :login
+    end
+  end
+
+  def end
+    session[:user_id] = nil
+    redirect_to root_path
+  end
 end
