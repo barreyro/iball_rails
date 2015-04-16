52b10c83213bfb5d58fa78d8631e445ab68e1c4d
Stephanie <stephaniebarreyro@gmail.com>
2015-04-15 20:55:30 -0700

    Rename Session#controller methods.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,20 @@ class SessionsController < ApplicationController
   def login
   end
 
-  def begin
+  def create
     user = Session.login_by_username_or_email(params[:user_input])
 
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:success] = "Welcome, #{user.username}!"
+      flash[:success] = "Welcome back, #{user.username}!"
       redirect_to root_path
     else
-      flash[:failure] = "Login failed."
-      render :login
+      flash[:failure] = "Login Failed."
+      redirect_to login_path
     end
   end
 
-  def end
+  def logout
     session[:user_id] = nil
     redirect_to root_path
   end
