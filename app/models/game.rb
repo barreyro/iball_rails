3e09410c6ca9ccfa0cf9566db7227da24d55bcd8
Stephanie <stephaniebarreyro@gmail.com>
2015-04-16 21:10:41 -0700

    Add search method to game#model.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,13 @@
 class Game < ActiveRecord::Base
   has_many   :comments
+
+  def self.search(date, state)
+    @found_games = []
+    @games = Game.all
+    @games.each do |event|
+      if event.date == date && event.state == state
+        @found_games << event
+      end
+    end
+  end
 end
