4959ed324c1b50ecc1551a14288997569b50eb44
Stephanie <stephaniebarreyro@gmail.com>
2015-04-17 23:38:29 -0700

    Add and style edit user form.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,18 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
   def show
     @user = User.find(params[:id])
   end
